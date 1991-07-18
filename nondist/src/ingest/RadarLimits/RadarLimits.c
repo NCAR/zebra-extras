5c7c0e110dc6a175a40dada596f0db918225571a
Jon Corbet <corbet@eol.ucar.edu>
1991-07-18 23:20:21 +0000

    Florida changes.
    svn path=/trunk/zebra/nondist; revision=885

---
+++ nondist/src/ingest/RadarLimits/RadarLimits.c
@@ -0,0 +1,468 @@
+/*
+ * Quickie hack to get in and store the limits of a radar's scanning.
+ */
+static char *rcsid = "$Id: RadarLimits.c,v 2.0 1991-07-18 23:07:02 corbet Exp $";
+
+# include <X11/Intrinsic.h>
+# include <X11/StringDefs.h>
+# include <X11/Shell.h>
+# include <X11/Xaw/Form.h>
+# include <X11/Xaw/Text.h>
+# include <X11/Xaw/Label.h>
+# include <X11/Xaw/Command.h>
+# include <X11/Xaw/AsciiText.h>
+
+# include "defs.h"
+# include <message.h>
+# include <DataStore.h>
+# include <dm.h>
+
+
+static XtAppContext Appc;
+Widget Top, Form, Shell, Left, Right, Last, Wm;
+bool Visible = FALSE;
+bool Override = TRUE;
+
+char LString[40], RString[40];
+
+DataObject DObj;
+
+
+# ifdef __STDC__
+	static int MsgHandler (Message *);
+	static void Store (void);
+	static int xevent (int);
+	static void CreateWidget (char *);
+	static void wm (void);
+	static void CreateDataObject (char *);
+# else
+# endif
+
+main (argc, argv)
+int argc;
+char **argv;
+{
+	Arg args[5];
+	int n;
+	char *platform;
+/*
+ * Hook in.
+ */
+	if (! msg_connect (MsgHandler, "RadarLimits"))
+		exit (1);
+	usy_init ();
+	ds_Initialize ();
+/*
+ * Get set up with the toolkit.
+ */
+	Top = XtAppInitialize (&Appc, "RadarLimits", NULL, 0, &argc, argv,
+		NULL, NULL, 0);
+	platform = (argc > 1) ? argv[1] : "cp2";
+/*
+ * Create our shell.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNinput, True);	n++;
+	XtSetArg (args[n], XtNoverrideRedirect, True);	n++;
+	Shell = XtCreatePopupShell ("Radar Limits", topLevelShellWidgetClass,
+		Top, args, n);
+/*
+ * Create the rest of the widget.
+ */
+	CreateWidget (platform);
+/*
+ * Create the static parts of our data object.
+ */
+	CreateDataObject (platform);
+/*
+ * Tell msglib about our X connection.
+ */
+	msg_add_fd (XConnectionNumber (XtDisplay (Shell)), xevent);
+/*
+ * Now we just wait.
+ */
+	reconfig (100, 600, 300, 70);
+	sync ();
+	xevent (0);
+	msg_await ();
+}
+
+
+
+
+
+static void
+CreateWidget (platform)
+char *platform;
+/*
+ * Create the widget itself.
+ */
+{
+	char string[80];
+	Arg args[15];
+	Widget w, label, title;
+	int n;
+/*
+ * Put a form inside it.
+ */
+	Form = XtCreateManagedWidget ("form", formWidgetClass, Shell, args, 0);
+/*
+ * Start with a label.
+ */
+	sprintf (string, "Radar azimuth limits for %s", platform);
+	n = 0;
+	XtSetArg (args[n], XtNlabel, string);		n++;
+	XtSetArg (args[n], XtNfromHoriz, NULL);		n++;
+	XtSetArg (args[n], XtNfromVert, NULL);		n++;
+	title = XtCreateManagedWidget("title",labelWidgetClass, Form, args, n);
+/*
+ * The dm/wm button.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, "Ctl: DM"  );	n++;
+	XtSetArg (args[n], XtNfromHoriz, title);	n++;
+	XtSetArg (args[n], XtNfromVert, NULL);		n++;
+	Wm = XtCreateManagedWidget ("ctl", commandWidgetClass, Form, args, n);
+	XtAddCallback (Wm, XtNcallback, wm, 0);
+/*
+ * Another label for the left limit.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, "From"  );		n++;
+	XtSetArg (args[n], XtNfromHoriz, NULL);		n++;
+	XtSetArg (args[n], XtNfromVert, title);		n++;
+	label =XtCreateManagedWidget("lLeft", labelWidgetClass, Form, args, n);
+/*
+ * The text widget for the left limit.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, label);	n++;
+	XtSetArg (args[n], XtNfromVert, title); 		n++;
+	XtSetArg (args[n], XtNdisplayPosition, 0); 	n++;
+	XtSetArg (args[n], XtNinsertPosition, 0); 	n++;
+	XtSetArg (args[n], XtNresize, XawtextResizeNever); n++;
+	XtSetArg (args[n], XtNtype, XawAsciiString); 	n++;
+	XtSetArg (args[n], XtNuseStringInPlace, True); 	n++;
+	XtSetArg (args[n], XtNlength, 40); 		n++;
+	XtSetArg (args[n], XtNstring, LString);		n++;
+	XtSetArg (args[n], XtNleftMargin, 5);		n++;
+	XtSetArg (args[n], XtNeditType, XawtextEdit);	n++;
+	Left = XtCreateManagedWidget ("Left", asciiTextWidgetClass, Form,
+			args, n);
+/*
+ * Intervening label.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, "deg to"  );	n++;
+	XtSetArg (args[n], XtNfromHoriz, Left);		n++;
+	XtSetArg (args[n], XtNfromVert, title);		n++;
+	w = XtCreateManagedWidget ("lRight", labelWidgetClass, Form, args, n);
+/*
+ * The text widget for the right limit.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, w);	n++;
+	XtSetArg (args[n], XtNfromVert, title);		n++;
+	XtSetArg (args[n], XtNdisplayPosition, 0); 	n++;
+	XtSetArg (args[n], XtNinsertPosition, 0); 	n++;
+	XtSetArg (args[n], XtNresize, XawtextResizeNever); n++;
+	XtSetArg (args[n], XtNtype, XawAsciiString); 	n++;
+	XtSetArg (args[n], XtNuseStringInPlace, True); 	n++;
+	XtSetArg (args[n], XtNlength, 10); 		n++;
+	XtSetArg (args[n], XtNstring, RString);		n++;
+	XtSetArg (args[n], XtNleftMargin, 5);		n++;
+	XtSetArg (args[n], XtNeditType, XawtextEdit);	n++;
+	Right = XtCreateManagedWidget ("Right", asciiTextWidgetClass, Form,
+			args, n);
+/*
+ * And something to finish.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, "deg."  );	n++;
+	XtSetArg (args[n], XtNfromHoriz, Right);		n++;
+	XtSetArg (args[n], XtNfromVert, title);		n++;
+	w = XtCreateManagedWidget ("lEnd", labelWidgetClass, Form, args, n);
+/*
+ * The store button
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, "Store"  );	n++;
+	XtSetArg (args[n], XtNfromHoriz, w);		n++;
+	XtSetArg (args[n], XtNfromVert, title);		n++;
+	w = XtCreateManagedWidget ("Store", commandWidgetClass, Form, args, n);
+	XtAddCallback (w, XtNcallback, Store, 0);
+/*
+ * A label widget to say when we last stored something.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNlabel, " "  );	n++;
+	XtSetArg (args[n], XtNfromHoriz, NULL);		n++;
+	XtSetArg (args[n], XtNfromVert, Left);		n++;
+	Last = XtCreateManagedWidget ("last", labelWidgetClass, Form, args, n);
+}
+
+
+
+static int
+xevent (fd)
+int fd;
+/*
+ * Deal with an Xt event.
+ */
+{
+	XEvent event;
+/*
+ * Deal with events as long as they keep coming.
+ */
+ 	while (XtAppPending (Appc))
+	{
+		XtAppNextEvent (Appc, &event);
+		XtDispatchEvent (&event);
+	}
+	return (0);
+}
+
+
+
+
+
+
+MsgHandler (msg)
+struct message *msg;
+/*
+ * Log a client event.
+ */
+{
+/*
+ * Maybe it's a display manager message.
+ */
+	if (msg->m_proto == MT_DISPLAYMGR)
+		dm_msg (msg->m_data);
+/*
+ * Everything else is assumed to be a message handler event.
+ */	
+	else if (msg->m_proto == MT_MESSAGE)
+	{
+		struct mh_template *tmpl = (struct mh_template *) msg->m_data;
+		if (tmpl->mh_type == MH_SHUTDOWN)
+			exit (0);
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+
+sync ()
+/*
+ * Synchronize with the window system.
+ */
+{
+	XSync (XtDisplay (Top), False);
+}
+
+
+
+
+
+reconfig (x, y, w, h)
+int x, y, w, h;
+/*
+ * Reconfigure the window.
+ */
+{
+	Arg args[5];
+
+	XtSetArg (args[0], XtNx, x);
+	XtSetArg (args[1], XtNy, y);
+	XtSetArg (args[2], XtNwidth, w);
+	XtSetArg (args[3], XtNheight, h);
+	XtSetValues (Shell, args, 4);
+/* 
+ * If they can't see us yet, make it so now.
+ */
+	if (! Visible)
+	{
+		XtPopup (Shell, XtGrabNone);
+		Visible = TRUE;
+	}
+	sync ();
+}
+
+
+
+
+
+static void
+wm ()
+/*
+ * Try to change override redirect.
+ */
+{
+	Arg args[2];
+/*
+ * If the window is up, take it down.
+ */
+	if (Visible)
+		XtPopdown (Shell);
+/*
+ * Set the parameter.
+ */
+	Override = ! Override;
+	XtSetArg (args[0], XtNoverrideRedirect, Override);
+	XtSetValues (Shell, args, 1);
+/*
+ * Set the label on the command widget too.
+ */
+	if (Override)
+		XtSetArg (args[0], XtNlabel, "Ctl: DM");
+	else
+		XtSetArg (args[0], XtNlabel, "Ctl: WM");
+	XtSetValues (Wm, args, 1);
+/*
+ * Put the window back if it was before.
+ */
+	if (Visible)
+		XtPopup (Shell, XtGrabNone);
+}
+
+
+
+
+dm_msg (dmsg)
+struct dm_msg *dmsg;
+/*
+ * Deal with a DM message.
+ */
+{
+	struct dm_hello reply;
+/*
+ * See what we got.
+ */
+	switch (dmsg->dmm_type)
+	{
+	/*
+	 * Maybe it's a DM scoping us out.
+	 */
+	   case DM_HELLO:
+	   	reply.dmm_type = DM_HELLO;
+		msg_send ("Displaymgr", MT_DISPLAYMGR, FALSE, &reply, 
+			sizeof (reply));
+		break;
+	/*
+	 * Maybe it's a reconfig.
+	 */
+	   case DM_RECONFIG:
+		if (Override)
+		   	reconfig (dmsg->dmm_x, dmsg->dmm_y, dmsg->dmm_dx,
+				dmsg->dmm_dy);
+		break;
+	/*
+	 * They might want us to go away entirely.
+	 */
+	   case DM_SUSPEND:
+	   	if (Visible)
+		{
+			Visible = FALSE;
+			XtPopdown (Shell);
+		}
+		break;
+	}
+}
+
+
+
+
+
+static void
+SetStatus (s)
+char *s;
+/*
+ * Set our status widget.
+ */
+{
+	Arg args[2];
+
+	XtSetArg (args[0], XtNlabel, s);
+	XtSetValues (Last, args, 1);
+	sync ();
+}
+
+
+
+static void
+Store ()
+/*
+ * Store the result.
+ */
+{
+	char s[80];
+	time t;
+/*
+ * Decode the values.
+ */
+	if (! sscanf (LString, "%f", DObj.do_data[0]))
+	{
+		SetStatus ("Bad LEFT limit");
+		return;
+	}
+	else if (! sscanf (RString, "%f", DObj.do_data[1]))
+	{
+		SetStatus ("Bad RIGHT limit");
+		return;
+	}
+/*
+ * Get the time and store everything.
+ */
+	tl_GetTime (&t);
+	DObj.do_begin = DObj.do_end = DObj.do_times[0] = t;
+	ds_PutData (&DObj, FALSE);
+/*
+ * Time for a message.
+ */
+	sprintf (s, "Last stored %d:%02d:%02d", t.ds_hhmmss/10000,
+		(t.ds_hhmmss/100) % 100, t.ds_hhmmss % 100);
+	SetStatus (s);
+}
+
+
+
+
+
+static void
+CreateDataObject (platform)
+char *platform;
+/*
+ * Make the data object for this platform.
+ */
+{
+	char pname[80];
+/*
+ * Create the real platform name and look it up.
+ */
+	strcpy (pname, platform);
+	strcat (pname, "-az-limits");
+	if ((DObj.do_id = ds_LookupPlatform (pname)) == BadPlatform)
+	{
+		msg_ELog (EF_EMERGENCY, "No az lims platform %s", pname);
+		exit (1);
+	}
+/*
+ * Start to fill everything in.
+ */
+	DObj.do_org = OrgScalar;
+	DObj.do_npoint = 1;
+	DObj.do_nbyte = 8;
+	DObj.do_times = ALLOC (time);
+	DObj.do_data[0] = (float *) malloc (2*sizeof (float));
+	DObj.do_data[1] = DObj.do_data[0] + 1;
+	DObj.do_nfield = 2;
+	DObj.do_fields[0] = "left";
+	DObj.do_fields[1] = "right";
+	DObj.do_flags = 0;
+	DObj.do_badval = -9999.0;
+}
---
+++ nondist/src/ingest/fm/fm_ingest.c
@@ -0,0 +1,296 @@
+/*
+ * Field mill data ingest.
+ */
+
+# include <defs.h>
+# include <message.h>
+# include <timer.h>
+# include <DataStore.h>
+# include "SLGrabber.h"
+
+
+
+/*
+ * The following table is used to map lines onto different types of 
+ * stuff.  The field mill line returns a lot of different data, and
+ * we need to get the right thing into the right place.
+ */
+
+# ifdef __STDC__
+	void	InitFMill (char *);
+# else
+	void	InitFMill ();
+# endif
+
+struct init_line
+{
+	char *il_begin;		/* Beginning of the line	*/
+	void (*il_func)();	/* Function to begin ingest	*/
+} InitLines[] =
+{
+	{ "FIELD MILL GRID",	InitFMill	},
+	{ 0,			0		}
+};
+
+
+/*
+ * If we are currently in a particular type of data, this function is
+ * the one to handle input.
+ */
+
+void (*LineFunc) () = 0;
+
+/*
+ * Stuff for field mill ingest.
+ */
+# define FMXRES 25
+# define FMYRES 25
+DataObject FMDobj;
+float FMGrid[FMXRES * FMYRES];
+int FMXpos, FMYpos;
+time FMTime;
+bool FMBlank = FALSE;		/* Blank line expected	*/
+
+
+/*
+ * Other forward funcs.
+ */
+# ifdef __STDC__
+	int	MsgHandler (Message *);
+	void	NewData	(char *);
+	void	FMData (char *);
+	void	SetupFMill (void);
+	void	FMDone (void);
+# else
+# endif
+
+
+
+main (argc, argv)
+int argc;
+char **argv;
+{
+/*
+ * Hook up.
+ */
+	usy_init ();
+	if (! msg_connect (MsgHandler, "FMIngest"))
+	{
+		printf ("Unable to connect to message handler\n");
+		exit (1);
+	}
+	ds_Initialize ();
+/*
+ * Initializations.
+ */
+	SetupFMill ();
+/*
+ * Wait for lines.
+ */
+	msg_await ();
+}
+
+
+
+
+int
+MsgHandler (msg)
+Message *msg;
+/*
+ * Input!
+ */
+{
+/*
+ * Look for serial data.
+ */
+	if (msg->m_proto == MT_SLDATA)
+		NewData (((sldata *) msg->m_data)->sl_data);
+/*
+ * Everything else is assumed to be a message handler event.
+ */	
+	else if (msg->m_proto == MT_MESSAGE)
+	{
+		struct mh_template *tmpl = (struct mh_template *) msg->m_data;
+		if (tmpl->mh_type == MH_SHUTDOWN)
+			Die ();
+	}
+	return (0);
+}
+	
+
+
+
+void
+NewData (line)
+char *line;
+/*
+ * A line of serial data has arrived.
+ */
+{
+	int len, i;
+/*
+ * If there is currently a data function, we're in the middle of something
+ * and we need to just pass it off.
+ */
+	if (LineFunc)
+	{
+		(*LineFunc) (line);
+		return;
+	}
+/*
+ * Otherwise we need to try to match something.
+ */
+	for (i = 0; InitLines[i].il_begin; i++)
+		if (! strncmp (InitLines[i].il_begin, line,
+					strlen (InitLines[i].il_begin)))
+			break;
+	if (InitLines[i].il_begin)
+		(*InitLines[i].il_func) (line);
+}
+
+
+
+
+void
+SetupFMill ()
+/*
+ * Initial setup for field mills.
+ */
+{
+	RGrid *rg = &FMDobj.do_desc.d_rgrid;
+/*
+ */
+ 	if ((FMDobj.do_id = ds_LookupPlatform ("field-mill")) == BadPlatform)
+	{
+		msg_ELog (EF_EMERGENCY, "Field mill platform unknown");
+		exit (1);
+	}
+	FMDobj.do_org = Org2dGrid;
+	FMDobj.do_npoint = 1;
+	FMDobj.do_nbyte = FMXRES * FMYRES * sizeof (float);
+/* 
+ * Hardwired grid info.
+ */
+	rg->rg_nX = FMXRES;
+	rg->rg_nY = FMYRES;
+	rg->rg_nZ = 0;
+	rg->rg_Xspacing = rg->rg_Yspacing = 1.389;	/* yes, really */
+/*
+ * Locations.
+ */
+	FMDobj.do_loc.l_lat = 28.39107;
+	FMDobj.do_loc.l_lon = -80.84413;
+/*
+ * Other stuff.
+ */
+	FMDobj.do_times = &FMTime;
+	FMDobj.do_data[0] = FMGrid;
+	FMDobj.do_nfield = 1;
+	FMDobj.do_fields[0] = "fstrength";
+	FMDobj.do_flags = 0;
+	FMDobj.do_badval = -99999;
+}
+
+
+
+
+
+void
+InitFMill (line)
+char *line;
+/*
+ * Get started on field mill data.
+ */
+{
+	int h, m, s;
+/*
+ * Parse out the begin time.
+ *
+ * FIELD MILL GRID         13:26:00  07/13/1991
+ */
+	tl_GetTime (&FMDobj.do_begin);
+	if (sscanf (line + 20, "%d:%d:%d", &h, &m, &s) < 3)
+	{
+		msg_ELog (EF_PROBLEM, "Bad FMILL init line: '%s'", line);
+		return;
+	}
+	FMDobj.do_begin.ds_hhmmss = h*10000 + m*100 + s;
+	FMDobj.do_times[0] = FMDobj.do_end = FMDobj.do_begin;
+/*
+ * Now we just set up our line function.
+ */
+	FMXpos = FMYpos = 0;
+	FMBlank = FALSE;
+	LineFunc = FMData;
+}
+
+
+
+int
+Die ()
+{
+	exit (0);
+}
+
+void
+FMData (line)
+char *line;
+/*
+ * Deal with a line of field mill data.
+ */
+{
+	int n[5], i;
+/*
+ * If we're expecting a blank line, check for it.
+ */
+	if (FMBlank)
+	{
+		if (strlen (line) > 10)
+		{
+			msg_ELog (EF_PROBLEM,"Got '%s' instead of blank",line);
+			LineFunc = 0;
+		}
+		FMBlank = FALSE;
+		return;
+	}
+/*
+ * Try to scan out some numbers.
+ */
+	if (sscanf (line, "%d %d %d %d %d", n, n + 1, n + 2, n + 3, n + 4) < 5)
+	{
+		msg_ELog (EF_PROBLEM, "Bad fmill line: '%s'", line);
+		LineFunc = 0;
+		return;
+	}
+/*
+ * Store them.
+ */
+	for (i = 0; i < 5; i++)
+		FMGrid[FMXpos*FMXRES + FMYpos++] = ((float) n[i])/1000.0;
+/*
+ * See where we are.
+ */
+	if (FMYpos >= FMYRES)
+	{
+		FMXpos++;
+		FMYpos = 0;
+		if (FMXpos >= FMXRES)
+			FMDone ();
+		else
+			FMBlank = TRUE;
+	}
+}
+
+
+
+
+
+void
+FMDone ()
+/*
+ * We're finished with field mill data.
+ */
+{
+	ds_PutData (&FMDobj, FALSE);
+	LineFunc = 0;
+}
