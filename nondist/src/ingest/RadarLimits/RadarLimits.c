4d65295a48f9adcb2ea569bf0a05b79f5ae385d2
Jon Corbet <corbet@eol.ucar.edu>
1991-09-13 15:01:58 +0000

    Added the copyright notice.
    svn path=/trunk/zebra/nondist; revision=925

--- nondist/src/ingest/RadarLimits/RadarLimits.c
+++ nondist/src/ingest/RadarLimits/RadarLimits.c
@@ -1,7 +1,24 @@
 /*
  * Quickie hack to get in and store the limits of a radar's scanning.
  */
-static char *rcsid = "$Id: RadarLimits.c,v 2.0 1991-07-18 23:07:02 corbet Exp $";
+static char *rcsid = "$Id: RadarLimits.c,v 2.1 1991-09-13 15:01:58 corbet Exp $";
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 
 # include <X11/Intrinsic.h>
 # include <X11/StringDefs.h>
@@ -13,6 +30,7 @@ static char *rcsid = "$Id: RadarLimits.c,v 2.0 1991-07-18 23:07:02 corbet Exp $"
 # include <X11/Xaw/AsciiText.h>
 
 # include "defs.h"
+# include "copyright.h"
 # include <message.h>
 # include <DataStore.h>
 # include <dm.h>
