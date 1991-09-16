9329ae60559df461c6320f302dfbe27975993bdc
Chris Burghart <burghart@ucar.edu>
1991-09-16 21:32:17 +0000

    added copyright stuff
    svn path=/trunk/zebra/nondist; revision=928

--- nondist/src/ingest/fm/fm_ingest.c
+++ nondist/src/ingest/fm/fm_ingest.c
@@ -1,7 +1,28 @@
 /*
  * Field mill data ingest.
  */
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 
+
+static char *rcsid = "$Id: fm_ingest.c,v 2.1 1991-09-16 21:31:20 burghart Exp $";
+
+# include <copyright.h>
 # include <defs.h>
 # include <message.h>
 # include <timer.h>
