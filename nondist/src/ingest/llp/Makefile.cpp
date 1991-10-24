c71d1aba8bd4924fd550fd6f1a91397f924e00a5
Jon Corbet <corbet@eol.ucar.edu>
1991-10-24 22:49:29 +0000

    parameterization.
    svn path=/trunk/zebra/nondist; revision=1042

--- nondist/src/ingest/llp/Makefile.cpp
+++ nondist/src/ingest/llp/Makefile.cpp
@@ -15,29 +15,26 @@
  * through use or modification of this software.  UCAR does not provide 
  * maintenance or updates for its software.
  */
-MFVERSION="$Id: Makefile.cpp,v 1.2 1991-09-26 18:03:58 gracio Exp $"
+MFVERSION="$Id: Makefile.cpp,v 1.3 1991-10-24 22:49:29 corbet Exp $"
+
+# include "../../include/config.h"
 
 # ifdef sun
 /*
  * Sun options
  */
-CC=gcc
-CFLAGS=  -g -O -I$(ZEBHOME)/fcc/include -I$(ZEBHOME)/rdss/include
-LIBS=$(ZEBHOME)/fcc/lib/libfcc.a -lnetcdf -lrdss -lXaw -lXt -lXmu -lXext -lX11 -ltermcap -lm
+CC=CCompiler
+CFLAGS= CCOptions -I$(FCCINC) -I$(RDSSINC)
+LIBS=ZebLibrary -lnetcdf -lrdss -lXaw -lXmu -lXt -lXext -lX11 -ltermcap -lm
 # endif
-
-
-BINDIR=../../bin
-LIBDIR=../../lib
-HDIR=../../include
 
 OBJS = llp_ingest.o
 
 all:	llp_ingest llp_ingest.lf
 
 install:	llp_ingest llp_ingest.lf
-	install -c llp_ingest $(BINDIR)
-	install -c -m 0444 llp_ingest.lf $(LIBDIR)
+	install -c llp_ingest D_BINDIR
+	install -c -m 0444 llp_ingest.lf D_LIBDIR
 
 include:
 
@@ -48,7 +45,7 @@ llp_ingest.lf: llp_ingest.state
 	uic < make-lf
 
 clean:
-	rm -f *~ llp_ingest *.o Makefile.bak
+	rm -f *~ llp_ingest *.o Makefile.bak llp_ingest.lf
 
 Makefile: mf
 
@@ -56,7 +53,7 @@ mf:
 	mv Makefile Makefile~
 	cp Makefile.cpp Makefile.c
 	echo "# DO NOT EDIT -- EDIT Makefile.cpp INSTEAD" > Makefile
-	cc -E Makefile.c >> Makefile
+	cc -E -DMAKING_MAKEFILE Makefile.c | cat -s >> Makefile
 	rm -f Makefile.c
 	make depend
 
