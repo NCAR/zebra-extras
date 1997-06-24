56f66a50ea48e5bdc5600cd0c6b1a911ff75815b
Gary Granger <granger@ucar.edu>
1997-06-24 17:19:26 +0000

    override a local DISPLAY and set it to localhost:0 to avoid shared memory
    svn path=/trunk/zebra/project; revision=6076

--- project/STM/src/proj_env.cpp
+++ project/STM/src/proj_env.cpp
@@ -1,11 +1,23 @@
 XCOMM !/bin/csh -f
 XCOMM
 XCOMM Environment variable settings specific to ARM
-XCOMM $Id: proj_env.cpp,v 1.3 1997-06-02 22:55:31 granger Exp $
+XCOMM $Id: proj_env.cpp,v 1.4 1997-06-24 17:19:26 granger Exp $
 
 #include "config.h"
 
 echo "Reading proj_env"
+
+setenv HOST `uname -n`
+if ( ! $?DISPLAY ) then
+	setenv DISPLAY localhost:0
+	echo "Setting DISPLAY to localhost:0"
+else
+	set display=`echo $DISPLAY | sed -e 's/:.*//'`
+	if ( x$display == x || x$display == x$HOST ) then
+		setenv DISPLAY localhost:0
+		echo "Setting DISPLAY to localhost:0"
+	endif
+endif
 
 XCOMM Simple configuration settings used by zstart
 set dm="dm -single "
@@ -15,7 +27,6 @@ if (! $?ZEB_EVENTLOGGER) \
 	setenv ZEB_EVENTLOGGER 'EventLogger -w'
 
 setenv ZEBHOME $ZEB_TOPDIR
-setenv HOST `uname -n`
 setenv CONFIGHOME CFG_PROJECTDIR
 setenv ZEB_PROJDIR $CONFIGHOME
 setenv DATA_DIR CFG_DEFAULTDATADIR
